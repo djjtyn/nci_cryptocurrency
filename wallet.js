c2b3aa5dc63faee93d1499721bb3f8a987300ce5
Your Name <djjtynan@gmail.com>
2021-12-08 11:27:01 +0000

    wallet displaying public/private keys

--- wallet.js
+++ wallet.js
@@ -1,2 +1,12 @@
 const WALLET = require("ethereumjs-wallet").default;
-const KECCAK256 = require("keccak256")
+const KECCAK256 = require("keccak256");
+
+const GETWALLETDETAILS = async() => {
+    console.log("Getting wallet details");
+    const myWallet = WALLET.generate();
+    console.log("Wallet generated");
+    console.log("Wallet public key: " + myWallet.getPublicKeyString());
+    console.log("Wallet private key: " + myWallet.getPrivateKeyString())
+}
+
+GETWALLETDETAILS();

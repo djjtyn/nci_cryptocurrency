ddca01a756c4ab710307beaf3154b6c6970f34ea
Your Name <djjtynan@gmail.com>
2021-12-08 11:37:33 +0000

    Eth address logging to console

--- wallet.js
+++ wallet.js
@@ -3,10 +3,18 @@ const KECCAK256 = require("keccak256");
 
 const GETWALLETDETAILS = async() => {
     console.log("Getting wallet details");
+    //Generate public/private keys
     const myWallet = WALLET.generate();
     console.log("Wallet generated");
     console.log("Wallet public key: " + myWallet.getPublicKeyString());
     console.log("Wallet private key: " + myWallet.getPrivateKeyString())
+
+    const pubKey = myWallet.getPublicKey();
+    const hashedPublicKey = KECCAK256(pubKey).toString('hex')
+    console.log("Hashed public key: 0X" + pubKey.toString('hex'));
+
+    let ethAddress = hashedPublicKey.substring(24);
+    console.log("Eth address is 0x" + ethAddress);
 }
 
 GETWALLETDETAILS();
